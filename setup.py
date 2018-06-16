91077f7624773cdbcdd3f31c18c035dd74cc1365
zero323 <mszymkiewicz@gmail.com>
2018-06-16 21:00:33 +0200

    Add description and long description

--- setup.py
+++ setup.py
@@ -12,6 +12,8 @@ setup(
     license='Apache 2.0',
     author='zero323',
     author_email='',
-    description='',
+    description='A proof of concept asynchronous actions for PySpark using concurent.futures',
+    long_description=(open('README.md').read() if exists('README.md')
+                        else ''), 
     install_requires=install_requires
 )
