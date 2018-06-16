e367890f507f65b6ef6910839b758cd9f5e421f8
zero323 <mszymkiewicz@gmail.com>
2018-06-16 21:41:56 +0200

    Add pyspark install to travis build

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 import sys
+import os
 from setuptools import setup, find_packages
 
 
 install_requires = [] if sys.version_info >= (3, 2) else ["futures"]
 
 setup(
-    name='asyncactions',
+    name='pyspark-asyncactions',
     version='0.0.1',
     packages=find_packages("."),
-    url='https://github.com/zero323/pyspark-async-actions',
+    url='https://github.com/zero323/pyspark-asyncactions',
     license='Apache 2.0',
     author='zero323',
     author_email='',
     description='A proof of concept asynchronous actions for PySpark using concurent.futures',
-    long_description=(open('README.md').read() if exists('README.md')
+    long_description=(open('README.md').read() if os.path.exists('README.md')
                         else ''), 
     install_requires=install_requires
 )
